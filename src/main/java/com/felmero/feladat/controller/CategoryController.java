3871fd2a0a6783a2ac321dab4ee2660a5b77266a
pavliczadam <pbobo3@gmail.com>
2019-10-10 13:22:17 +0200

    kategória get

--- src/main/java/com/felmero/feladat/controller/CategoryController.java
+++ src/main/java/com/felmero/feladat/controller/CategoryController.java
@@ -1,8 +1,11 @@
 package com.felmero.feladat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +31,18 @@ public class CategoryController {
 			return ResponseEntity.ok(categoryService.save(category));
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().build();
+		}
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseBody
+	public ResponseEntity<Category> getCategory(@PathVariable(value = "id") Long id) {
+		try {
+			Category category = categoryService.get(id);
+			return ResponseEntity.ok(category);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
 		}
 	}
 
